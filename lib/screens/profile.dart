e501e24c3ed4739af2e7cf93afe003fc22b7f47f
Alvinalia <133779368+Alvinalia@users.noreply.github.com>
2023-06-09 23:09:41 +0700

    Update profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Container(
